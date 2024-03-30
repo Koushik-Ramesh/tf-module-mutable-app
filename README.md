3794060b16b53655791202cb5a650baf7a2b172b
Koushik-Ramesh <koushik.r9380@gmail.com>
2024-03-31 01:33:33 +0530

    Autopush

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # tf-module-mutable-app
+
+This Repository is a backend module for all the components. This Contains the TERRAFORM CODE needed to provision the MUTABLE INFRASTRUCTURE
+
+All the needed values and configuration will be sourced from the respective component/mutable-infra
---
+++ ec2.tf
@@ -0,0 +1,29 @@
+# Creates the Spot instance on EC2
+resource "aws_spot_instance_request" "spot" {
+    count                       = var.SPOT_INSTANCE_COUNT
+    ami                         = data.aws_ami.ami.image_id
+    spot_price                  = "0.03"
+    instance_type               = var.SPOT_INSTANCE_TYPE
+    wait_for_fulfillment        = true
+    vpc_security_group_ids      = [aws_security_group.allows_app.id]
+    subnet_id                   = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS, count.index)
+  tags = {
+    Name = "${var.COMPONENT}-${var.ENV}"
+  }
+}
+
+# Creates On-Demand Instance
+resource "aws_instance" "OD" {
+  count         = var.OD_INSTANCE_COUNT
+  ami           = data.aws_ami.ami.image_id
+  instance_type = var.OD_INSTANCE_TYPE
+  vpc_security_group_ids = [aws_security_group.allows_app.id]
+  subnet_id                   = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS, count.index)
+  
+
+  tags = {
+    Name = "${var.COMPONENT}-${var.ENV}"
+  }
+}
+
+# If this is called by frontend component, then it has to be created in public subnet, if not in PRIVATE subnet
