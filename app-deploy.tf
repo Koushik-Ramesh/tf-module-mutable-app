4b3af45f170b276a6b4a65548bbdd7c1b9505483
Koushik-Ramesh <koushik.r9380@gmail.com>
2024-03-31 02:29:47 +0530

    Autopush

--- app-deploy.tf
+++ app-deploy.tf
@@ -1,19 +1,19 @@
-# Installing the application
-resource "null_resource" "app" {
-    count = local.INSTANCE_COUNT
-    connection {
-    type     = "ssh"
-    user     = local.SSH_USERNAME
-    password = local.SSH_PASSWORD
-    host     = element(local_INSTANCE_PRIVATE_IPS, count.index)
-  }
+# # Installing the application
+# resource "null_resource" "app" {
+#     count = local.INSTANCE_COUNT
+#     connection {
+#     type     = "ssh"
+#     user     = local.SSH_USERNAME
+#     password = local.SSH_PASSWORD
+#     host     = element(local_INSTANCE_PRIVATE_IPS, count.index)
+#   }
 
-  provisioner "remote-exec" {
-    inline = [
-      "puppet apply",
-      "consul join ${aws_instance.web.private_ip}",
-    ]
-  }
-}
+#   provisioner "remote-exec" {
+#     inline = [
+#       "puppet apply",
+#       "consul join ${aws_instance.web.private_ip}",
+#     ]
+#   }
+# }
        
-}
+
