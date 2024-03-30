89782d2ee327bc8c2399268626efe18678e07666
Koushik-Ramesh <koushik.r9380@gmail.com>
2024-03-31 02:16:10 +0530

    Autopush

--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
-local {
+locals {
     INSTANCE_IDS    = concat(aws_instance.OD.*.id , aws_spot_instance_request.spot.*.spot_instance_id)
     INSTANCE_COUNT  = var.OD_INSTANCE_COUNT + var.SPOT_INSTANCE_COUNT
 }
