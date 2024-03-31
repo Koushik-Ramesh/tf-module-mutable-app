9023d875bae47cbce847ceeff655aa0cae8a93dc
Koushik-Ramesh <koushik.r9380@gmail.com>
2024-04-01 01:16:49 +0530

    Autopush

--- listener.tf
+++ listener.tf
@@ -1,22 +1,9 @@
-# Creates a listener and attaches to the private ALB
-resource "aws_lb_listener" "private" {
-    count               = var.INTERNAL ? 1 : 0        
-    load_balancer_arn   = data.terraform_remote_state.alb.outputs.PRIVATE_ALB_ARN 
-    port                = "80"
-    protocol            = "HTTP"
-
-  default_action {
-    type                = "forward"
-    target_group_arn    = aws_lb_target_group.app.arn 
-  }
-}
 # Generates a random number in the given range
 resource "random_integer" "priority" {
   min = 100
   max = 500
   
 }
-
 # Creates the rule in the Private Loadbalancer Listener
 
 resource "aws_lb_listener_rule" "app_rule" {
