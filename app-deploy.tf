903104fa31224dfd7ebeb09b323f87e5a66b6398
Koushik-Ramesh <koushik.r9380@gmail.com>
2024-03-31 14:58:34 +0530

    Autopush

--- app-deploy.tf
+++ app-deploy.tf
@@ -1,19 +1,19 @@
-# # Installing the application
-# resource "null_resource" "app" {
-#     count = local.INSTANCE_COUNT
-#     connection {
-#     type     = "ssh"
-#     user     = local.SSH_USERNAME
-#     password = local.SSH_PASSWORD
-#     host     = element(local_INSTANCE_PRIVATE_IPS, count.index)
-#   }
+# Installing the application
+resource "null_resource" "app" {
+    count = local.INSTANCE_COUNT
 
-#   provisioner "remote-exec" {
-#     inline = [
-#       "puppet apply",
-#       "consul join ${aws_instance.web.private_ip}",
-#     ]
-#   }
-# }
+    connection {
+    type     = "ssh"
+    user     = local.SSH_USERNAME
+    password = local.SSH_PASSWORD
+    host     = element(local_INSTANCE_PRIVATE_IPS, count.index)
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "ansible-pull -U https://github.com/Koushik-Ramesh/ansible.git -e ENV=dev -e COMPONENT=${var.COMPONENT} roboshop-pull.yml"
+    ]
+  }
+}
        
 
