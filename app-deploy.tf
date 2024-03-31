9aa7ec1d48bbbb79ffec46992c1f3cf1608b1d1d
Koushik-Ramesh <koushik.r9380@gmail.com>
2024-03-31 15:18:34 +0530

    Autopush

--- app-deploy.tf
+++ app-deploy.tf
@@ -1,19 +1,17 @@
-# # Installing the application
-# resource "null_resource" "app" {
-#     count = local.INSTANCE_COUNT
-
-#     connection {
-#     type     = "ssh"
-#     user     = local.SSH_USERNAME
-#     password = local.SSH_PASSWORD
-#     host     = element(local_INSTANCE_PRIVATE_IPS, count.index)
-#   }
-
-#   provisioner "remote-exec" {
-#     inline = [
-#       "ansible-pull -U https://github.com/Koushik-Ramesh/ansible.git -e ENV=dev -e COMPONENT=${var.COMPONENT} roboshop-pull.yml"
-#     ]
-#   }
-# }
+# Installing the application
+resource "null_resource" "app" {
+    count = local.INSTANCE_COUNT
+  provisioner "remote-exec" {
+      connection {
+        type     = "ssh"
+        user     = local.SSH_USERNAME
+        password = local.SSH_PASSWORD
+        host     = element(local_INSTANCE_PRIVATE_IPS, count.index)
+    }
+        inline = [
+            "ansible-pull -U https://github.com/Koushik-Ramesh/ansible.git -e ENV=dev -e COMPONENT=${var.COMPONENT} roboshop-pull.yml"
+        ]
+    }
+}
        
 
