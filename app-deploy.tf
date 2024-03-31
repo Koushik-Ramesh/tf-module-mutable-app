8bd9a368167daeed67fe3676183981011876c0be
Koushik-Ramesh <koushik.r9380@gmail.com>
2024-03-31 15:22:50 +0530

    Autopush

--- app-deploy.tf
+++ app-deploy.tf
@@ -1,12 +1,17 @@
 # Installing the application
 resource "null_resource" "app" {
+
+    triggers = {
+      always_run = "${timestamp()}"
+    }
+
     count = local.INSTANCE_COUNT
   provisioner "remote-exec" {
       connection {
         type     = "ssh"
         user     = local.SSH_USERNAME
         password = local.SSH_PASSWORD
-        host     = element(local_INSTANCE_PRIVATE_IPS, count.index)
+        host     = element(local.INSTANCE_PRIVATE_IPS, count.index)
     }
         inline = [
             "ansible-pull -U https://github.com/Koushik-Ramesh/ansible.git -e ENV=dev -e COMPONENT=${var.COMPONENT} roboshop-pull.yml"
