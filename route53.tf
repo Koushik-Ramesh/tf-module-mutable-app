e7e433c2766a363156c4dbb934fdfe83706fcd5e
Koushik-Ramesh <koushik.r9380@gmail.com>
2024-03-31 21:13:29 +0530

    Autopush

--- route53.tf
+++ route53.tf
@@ -1,8 +1,8 @@
 # Creates App Component Record agaonst the ALB
 resource "aws_route53_record" "docdb_dns" {
-  zone_id = var.INTERNAL ? data.terraform_remote_state.alb.outputs.PRIVATE_ALB_ARN : data.terraform_remote_state.alb.outputs.PUBLIC_ALB_ARN
+  zone_id = var.INTERNAL ? data.terraform_remote_state.alb.outputs.PRIVATE_HOSTED_ZONE_ID : data.terraform_remote_state.alb.outputs.PUBLIC_HOSTED_ZONE_ID
   name    = "${var.COMPONENT}-${var.ENV}"
   type    = "CNAME"
   ttl     = 10
-  records = var.INTERNAL ? [ data.terraform_remote_state.alb.outputs.PRIVATE_ALB_ARN ] : [ data.terraform_remote_state.alb.outputs.PUBLIC_ALB_ARN ]
+  records = var.INTERNAL ? [ data.terraform_remote_state.alb.outputs.PRIVATE_ALB_ADDRESS ] : [ data.terraform_remote_state.alb.outputs.PUBLIC_ALB_ADDRESS ]
 }
