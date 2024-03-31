75c44941d83e1b0e0af93a02a3d320404672d0eb
Koushik-Ramesh <koushik.r9380@gmail.com>
2024-03-31 15:31:46 +0530

    Autopush

--- app-deploy.tf
+++ app-deploy.tf
@@ -15,7 +15,7 @@ resource "null_resource" "app" {
     }
         inline = [
             "sleep 30" ,
-            "ansible-pull -U https://github.com/Koushik-Ramesh/ansible.git -e ENV=dev -e COMPONENT=${var.Component} roboshop-pull.yml"
+            "ansible-pull -U https://github.com/Koushik-Ramesh/ansible.git -e ENV=dev -e COMPONENT=${var.COMPONENT} roboshop-pull.yml"
         ]
     }
 }
