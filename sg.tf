ae2bef6c10984d8c2efb6dd419d5a05500e9f76f
Koushik-Ramesh <koushik.r9380@gmail.com>
2024-05-05 22:01:31 +0530

    Autopush

--- sg.tf
+++ sg.tf
@@ -21,7 +21,7 @@ resource "aws_security_group" "allows_app" {
         cidr_blocks = [data.terraform_remote_state.vpc.outputs.VPC_CIDR, data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]
     }
 
-        ingress {
+    ingress {
         description = "Node Exporter Port"
         from_port = 9100
         to_port = 9100
